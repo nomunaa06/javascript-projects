0f85323ecd6528916aed3a26f857fe6c7d7cc800
nomunaa06 <118144792+nomunaa06@users.noreply.github.com>
2024-07-15 22:19:28 -0500

    Unit testing studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,70 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered:["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function launchOutput(num){
+if (num%2===0 && num%3===0 && num%5===0){
+    return 'LaunchCode Rocks!';
+}    
+else if(num%2===0 && num%3===0){
+    return 'LaunchCode!';
 }
-
+else if(num%3===0 && num%5 ===0){
+    return 'Code Rocks!';
+}
+else if(num%5===0 && num%2 ===0){
+    return 'Launch Rocks! (CRASH!!!!)';
+}
+else if (num%2===0){
+    return 'Launch!';
+}
+else if (num%3===0){
+    return 'Code!';
+}
+else if (num%5===0){
+    return 'Rocks!';
+}
+else {
+    return "Rutabagas! That doesn't work.";
+}
+}
+};
 module.exports = launchcode;
 
+// let launchcode = {
+//     organization: "nonprofit",
+//     executiveDirector: "Jeff",
+//     percentageCoolEmployees: 100,
+//     programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+//     launchOutput: function (num) {
+//       let output = '';
+    
+//       if (num%2 === 0){
+//         output += 'Launch';
+//       }
+    
+//       if (num%3 === 0){
+//         output += 'Code';
+//       }
+    
+//       if (num%5 === 0){
+//         if (output) {
+//           output += ' Rocks';
+//         } else {
+//           output += 'Rocks';
+//         }
+//       }
+    
+//       if (!output){
+//         return "Rutabagas! That doesn't work.";
+//       } else if (output==='Launch Rocks') {
+//         return output + '! (CRASH!!!!)';
+//       } else {
+//         return output + '!';
+//       }
+//     }
+//   }
+  
+//   module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,45 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
-
   // Write your unit tests here!
-  
+test("Organization has 'nonprofit'" , function(){
+  expect(launchcode.organization).toEqual("nonprofit");
+});
+test("Executive director is Jeff", function(){
+  expect(launchcode.executiveDirector).toEqual("Jeff");
+});
+test("pecrcentage of cool employee is number", function(){
+  expect(typeof launchcode.percentageCoolEmployees).toEqual("number");
+});  
+
+test("programms offered has Web Development,Data Analysis, Liftoff values", function(){
+  expect(launchcode.programsOffered).toContain("Web Development");
+  expect(launchcode.programsOffered).toContain("Data Analysis");
+  expect(launchcode.programsOffered).toContain("Liftoff");
+  expect(launchcode.programsOffered.length).toEqual(3);
+});  
+test("Divisible by 2 returns 'Launch!'", function(){
+  expect(launchcode.launchOutput(2)).toBe('Launch!');
+});  
+test("Divisible by 3 returns 'Code!'", function(){
+  expect(launchcode.launchOutput(3)).toBe('Code!');
+});  
+test("Divisible by 5 returns 'Rocks!'", function(){
+  expect(launchcode.launchOutput(5)).toBe('Rocks!');
+});  
+test("Divisible by 2 and 3 returns 'LaunchCode!'", function(){
+  expect(launchcode.launchOutput(6)).toBe('LaunchCode!');
+});  
+test("Divisible by 5 and 3 returns 'Code Rocks!'", function(){
+  expect(launchcode.launchOutput(15)).toBe('Code Rocks!');
+}); 
+test("Divisible by 5 and 2 returns 'Launch Rocks! (CRASH!!!!)'", function(){
+  expect(launchcode.launchOutput(10)).toBe('Launch Rocks! (CRASH!!!!)');
+}); 
+test("Divisible by 5, 3 and 2 returns 'LaunchCode Rocks!'", function(){
+  expect(launchcode.launchOutput(30)).toBe('LaunchCode Rocks!');
+});
+test("Not Divisible by 5, 3 and 2 returns 'Rutabagas! That doesn't work.'", function(){
+  expect(launchcode.launchOutput(31)).toBe("Rutabagas! That doesn't work.");
+});
 });
