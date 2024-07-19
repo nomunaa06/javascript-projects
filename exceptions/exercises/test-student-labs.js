3afc938f7ed42857189346cbf90df1ecd26986d0
Nomundari <nomunaa@gmail.com>
2024-07-18 23:41:17 -0500

    exceptions exercise

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,7 +1,11 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
+    let result;
+    try {
+      result = lab.runLab(3);
+    }
+    catch(e){ console.log(e.name, e.message);}
     console.log(`${lab.student} code worked: ${result === 27}`);
   }
 }
@@ -21,4 +25,29 @@ let studentLabs = [
   }
 ];
 
+
 gradeLabs(studentLabs);
+
+
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
