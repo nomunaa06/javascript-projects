cf7734429f7c6c1363f24336570ceb67950842e5
Nomundari <nomunaa@gmail.com>
2024-08-05 23:58:16 -0500

    dom exercises

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -3,7 +3,24 @@ function init () {
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
 
-    // Put your code for the exercises here.
+    button.addEventListener("click", event =>{
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+
+    missionAbort.addEventListener("mouseover", event=>{
+        missionAbort.style.backgroundColor = "red";
+    });
+
+    missionAbort.addEventListener("mouseout", event=>{
+        missionAbort.style.backgroundColor = "";
+    });
+    
+    missionAbort.addEventListener("click", event=>{
+        let response = window.confirm("Are you sure you want to abort the mission?");
+        if(response){
+            paragraph.innerHTML = "Mission aborted! Space shuttle returning home";
+        }
+    });
     
 }
 
