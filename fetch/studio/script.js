065ba33eab1e5a960e322fa2b7e84ff7faddad97
Nomundari <nomunaa@gmail.com>
2024-08-12 23:32:47 -0500

    Fetch studio without bonus part 2

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,33 @@
 //TODO: Add Your Code Below
+window.addEventListener('load', function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            json.sort((a,b)=>{
+                return b.hoursInSpace - a.hoursInSpace;
+            });
+            const container = document.getElementById('container');
+            let i=0;
+            while (i < 7) {
+                container.innerHTML += `<div class="astronaut">
+                                            <div class="bio">
+                                                <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                                                <ul>
+                                                <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                                <li>Active: ${json[i].active}</li>
+                                                <li>Skills: ${json[i].skills}</li>
+                                                </ul>
+                                            </div>
+                                            <img class="avatar" src="${json[i].picture}">
+                                        </div>`
+                i++;
+            }
+        
+            container.innerHTML +=`<div class="pageCount">Total number of Astronuat: ${i}</div>`
+           
+
+            
+        });
+    });
+    
+});
+
